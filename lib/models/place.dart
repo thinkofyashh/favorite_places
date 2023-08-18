5864f615259394c7d03b3257eda49dea7c362aa3
yashrawat <yashrawat682@gmail.com>
2023-08-18 06:29:54 +0530

    Displaying a location preview map snapshot via google.

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'dart:io';
-const  uuid=Uuid();
-class Place{
-  Place({required this.title,required this.image}) : id=uuid.v4();
+
+const uuid = Uuid();
+
+class PlaceLocation {
+  PlaceLocation(
+      {required this.latitude, required this.longitude, required this.address});
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
+class Place {
+  Place({required this.title, required this.image})
+      : id = uuid.v4();
   final String id;
   final String title;
   final File image;
+  //final PlaceLocation location;
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:favorite_places/models/place.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -10,8 +13,18 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   @override
-  Location? pickedLocation;
+
+  PlaceLocation? pickedLocation;
   var isgettinglocation = false;
+
+  String get locationImage{
+    if(pickedLocation==null){
+      return "";
+    }
+    final lat=pickedLocation!.latitude;
+    final long=pickedLocation!.longitude;
+    return "https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$long&key=AIzaSyBS-TMS4UDZxX75YPzSEpdS5md50-f8udY" ;
+  }
   void getcurrentlocation() async {
     Location location = Location();
     bool serviceEnabled;
@@ -35,13 +48,22 @@ class _LocationInputState extends State<LocationInput> {
       isgettinglocation = true;
     });
     locationData = await location.getLocation();
+    final lat=locationData.latitude;
+    final long=locationData.longitude;
+
+    if(lat ==null && long == null){
+      return;
+    }
+    final url=Uri.parse("https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=AIzaSyBS-TMS4UDZxX75YPzSEpdS5md50-f8udY");
+    final response=await http.get(url);
+    final resdata=json.decode(response.body);
+    final add=resdata['results'][0]['formatted_address'];
+
     setState(() {
+      pickedLocation=PlaceLocation(latitude: lat!, longitude: long!, address: add);
       isgettinglocation = false;
     });
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
-
   Widget build(BuildContext context) {
     Widget previewContent = Text(
       "Location not chosen",
@@ -50,6 +72,9 @@ class _LocationInputState extends State<LocationInput> {
             color: Theme.of(context).colorScheme.onBackground,
           ),
     );
+    if(pickedLocation!= null){
+      previewContent=Image.network(locationImage,fit: BoxFit.cover,height: double.infinity,width: double.infinity,);
+    }
     if (isgettinglocation) {
       previewContent = const CircularProgressIndicator();
     }
