4a800a1d45ddb46f1b96eda1dba95ee9c028df6a
yashrawat <yashrawat682@gmail.com>
2023-08-19 06:07:39 +0530

    Outputting the location data.

--- lib/screens/places_details.dart
+++ lib/screens/places_details.dart
@@ -5,6 +5,12 @@ class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({Key? key, required this.place}) : super(key: key);
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final long = place.location.longitude;
+    return "https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$long&key=AIzaSyBS-TMS4UDZxX75YPzSEpdS5md50-f8udY";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +30,36 @@ class PlaceDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
-          )
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 16),
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors.transparent, Colors.black54],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    child: Text(
+                      place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
